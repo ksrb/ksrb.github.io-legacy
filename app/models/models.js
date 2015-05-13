ad14e67c5af69c3705f69dd1940425d5dba9aefa
Kevin Suen <ksrbkevinsuen@gmail.com>
2015-05-12 17:50:41 -0700

    Skills section: added tooltip to legends Skills and Navbar section: adjust transition duration

--- app/models/models.js
+++ app/models/models.js
@@ -9,57 +9,68 @@
                 {
                     name: "java",
                     category: "programming",
-                    level: 8
+                    level: 8,
+                    site: "http://en.wikipedia.org/wiki/Java_%28programming_language%29"
                 },
                 {
                     name: "javascript",
                     category: "programming",
-                    level: 5
+                    level: 5,
+                    site: "http://en.wikipedia.org/wiki/JavaScript"
                 },
                 {
                     name: "gradle",
                     category: "programming",
-                    level: 4
+                    level: 4,
+                    site: "http://en.wikipedia.org/wiki/Gradle"
                 },
                 {
                     name: "gwt",
                     category: "programming",
-                    level: 6
+                    level: 6,
+                    site: "http://en.wikipedia.org/wiki/Google_Web_Toolkit"
                 },
                 {
                     name: "html",
                     category: "design",
-                    level: 7
+                    level: 7,
+                    site: "http://en.wikipedia.org/wiki/HTML"
                 },
                 {
                     name: "css",
                     category: "design",
-                    level: 7
+                    level: 7,
+                    site: "http://en.wikipedia.org/wiki/Cascading_Style_Sheets"
                 },
                 {
                     name: "photoshop",
                     category: "design",
-                    level: 8
+                    level: 8,
+                    site: "http://en.wikipedia.org/wiki/Adobe_Photoshop"
                 },
                 {
                     name: "illustrator",
                     category: "design",
-                    level: 5
+                    level: 5,
+                    site: "http://en.wikipedia.org/wiki/Adobe_Illustrator"
                 },
                 {
                     name: "eclipse",
                     category: "tools",
-                    level: 7
+                    level: 7,
+                    site: "http://en.wikipedia.org/wiki/Eclipse_%28software%29"
                 },
                 {
                     name: "intellij",
                     category: "tools",
-                    level: 6
+                    level: 6,
+                    site: "http://en.wikipedia.org/wiki/IntelliJ_IDEA"
                 },
                 {
                     name: "git",
                     category: "tools",
-                    level: 6
+                    level: 6,
+                    site: "http://en.wikipedia.org/wiki/Git_%28software%29"
                 }
             ];
 
@@ -67,27 +78,39 @@
             var legends = [
                 {
                     name: "beginner",
-                    level: 2
+                    level: 2,
+                    description: "Secondary use, would require significant training to use commercially.",
+                    colloquial: "Kind of know what I'm doing with it, this would be like how well I know php from using Wordpress casually."
                 },
                 {
                     name: "familiar",
-                    level: 4
+                    level: 4,
+                    description: "Used at least once professionally, some training required to use commercially.",
+                    colloquial: "As Gradle is the only skill on this list that applies, something I would gladly learn more about given the chance."
                 },
                 {
                     name: "proficient",
-                    level: 6
+                    level: 6,
+                    description: "Used multiple times professionally, minimal direction required to use commercially.",
+                    colloquial: "Something that I'm pretty good with and generally able to problems given some time."
                 },
                 {
                     name: "instructor",
-                    level: 8
+                    level: 8,
+                    description: "Taught professionally, capable of assisting team leaders commercially.",
+                    colloquial: "Some of these things I've used for years (Photoshop) and would love teaching them."
                 },
                 {
                     name: "expert",
-                    level: 10
+                    level: 10,
+                    description: "Capable of leading team commercially.",
+                    colloquial: "I consider this someone who has at least 3-5 years experience and has succeeded delivering several products and at least failed to deliver once."
                 },
                 {
                     name: "master",
-                    level: 12
+                    level: 12,
+                    description: "Capable of authoring book, significant contributions to field.",
+                    colloquial: "This is like Douglas Crock level, someone who is a member of W3C and shaping the future of the web."
                 }
             ];
 
