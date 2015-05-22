3613a797a599b5a20fbd720cb1d1c9786992d32d
Kevin Suen <ksrbkevinsuen@gmail.com>
2015-05-21 21:37:54 -0700

    Experience section: added divider between content and sample work Changed use of slick slider directive from element to attribute

--- app/directives/directives.js
+++ app/directives/directives.js
@@ -25,7 +25,10 @@
     angular.module("app")
         .directive('slickSlider', ["$timeout", function ($timeout) {
             return {
-                link: function (scope, element, attrs) {
+                /**
+                 * Timeout used to allow DOM to load before attempting to execute slick initialization see http://stackoverflow.com/a/24523463
+                 */
+                link: function link(scope, element, attrs) {
                     $timeout(function () {
                         $(element).slick(scope.$eval(attrs.data));
                     });
